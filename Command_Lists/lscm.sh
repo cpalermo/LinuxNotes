63eb70f7b7345d7aa2f86b9a661268cac23013c9
Carlo Palermo <carlo.palermo@hdati.com>
2015-06-10 17:38:20 +0200

    Command list script

---
+++ Command_Lists/lscm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CMLIST=$( dpkg -L $1 | grep "bin/" | sort | sed 's/.*bin[/]//' )
+
+CMMAN=$( echo $CMLIST | sed 's/ /\n/g' | sed 's/^/man -P cat /' )
+
+CMGREP=$( echo $CMLIST | sed 's/ / - |  /g ; s/^/  / ; s/$/ - /' )
+
+echo "Command list of package $1:"
+echo ""
+sh -c "$CMMAN" | egrep "$CMGREP" | sed 's/^[ ]*// ; s/ - /\n# / ; s/$/\n/'
+
+exit
