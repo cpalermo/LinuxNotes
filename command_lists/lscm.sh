fec63bedc195fbea030a5a4547414d8ad9be90d4
Carlo Palermo <carlo.palermo@hdati.com>
2015-08-06 14:45:49 +0200

    lscm new

--- command_lists/lscm.sh
+++ command_lists/lscm.sh
@@ -4,12 +4,10 @@
 
 CMLIST=$( dpkg -L $1 | grep "bin/" | sort | sed 's/.*bin[/]//' )
 
-CMMAN=$( echo $CMLIST | sed 's/ /\n/g' | sed 's/^/man -P cat /' )
-
-CMGREP=$( echo $CMLIST | sed 's/ / [-—] |  /g ; s/^/  / ; s/$/ - /' )
+CMWHATIS=$( "whatis" $CMLIST )
 
 echo "Command list of package \"$1\":"
 echo ""
-sh -c "$CMMAN" | egrep "$CMGREP" | sed 's/^[ ]*// ; s/ [-—] /\n# / ; s/$/\n/'
+echo "$CMWHATIS" | sed 's/$/\n/ ; s/ /\n# / ; s/)[ ]*-/) -/g'
 
 exit
